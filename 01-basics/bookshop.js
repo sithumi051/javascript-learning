841da23dca8e119872455737e801f262fa447583
sithumi051 <113745@foe.sjp.ac.lk>
2026-09-10 10:27:09 +0530

    Learn JavaScript basics

---
+++ 01-basics/bookshop.js
@@ -0,0 +1,3 @@
+console.log("Book price: 1200");
+console.log("Quantity: 4");
+console.log("Total price: " + (1200*4));
---
+++ 01-basics/index.js
@@ -0,0 +1,12 @@
+// console.log we can print the information to the console
+console.log("Hello, JavaScript!");
+console.log("My name is Sithumi");
+console.log("I am learning JavaScript");
+
+//console.log can use to calculate the values
+console.log(5+10);
+console.log(20-5);
+console.log(5*10);
+console.log(20/5);
+
+console.log(2500*3);
